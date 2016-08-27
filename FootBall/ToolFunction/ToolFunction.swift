33088e75388cdc97040feb7d3108b36a119c66cb
lighthouse2403 <haidang2403@gmail.com>
2016-08-27 19:08:07 +0700

    test

--- FootBall/ToolFunction/ToolFunction.swift
+++ FootBall/ToolFunction/ToolFunction.swift
@@ -25,7 +25,7 @@ class ToolFunction: NSObject {
                 completion(result: snapshot.value!);
                 
             }else{
-            
+            NSLog("test")
             
             }
             
